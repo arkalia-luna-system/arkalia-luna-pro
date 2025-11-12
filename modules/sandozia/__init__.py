7cf341dfbfe5eb4fe1439291d811437fb2c8416e
Athalia Siwek <arkalia.luna.system@gmail.com>
2025-11-12 22:06:34 +0100

    fix: correction import CrossModuleValidator dans sandozia/__init__.py
    - Changé de '.validators.crossmodule' vers 'modules.utils.validators.crossmodule_validator'
    - Corrige ModuleNotFoundError dans les tests CI
    - Phase 4.2 : migration CrossModuleValidator terminée

--- modules/sandozia/__init__.py
+++ modules/sandozia/__init__.py
@@ -15,7 +15,7 @@ from .analyzer.behavior import BehaviorAnalyzer
 from .core.sandozia_core import SandoziaCore
 from .reasoning.collaborative import CollaborativeReasoning
 from .utils.metrics import SandoziaMetrics
-from .validators.crossmodule import CrossModuleValidator
+from .validators import CrossModuleValidator
 
 __version__ = "3.0.0-phase2"
 __author__ = "Arkalia-LUNA System"
